1ad657fafdb9dcc6a11541ebd39063231b9139ce
Mihai Gătejescu <gus666xe@gmail.com>
2019-02-01 19:11:58 +0200

    Add more attributes examples

--- attributes.c
+++ attributes.c
@@ -1,5 +1,7 @@
 #include <ncurses.h>
 
+#define LINES_NO 4
+
 int main (void)
 {
     int row, col;
@@ -7,21 +9,30 @@ int main (void)
     initscr();
     getmaxyx(stdscr, row, col);
 
+    attron(A_STANDOUT);
+    move((row - LINES_NO ) / 2, (col - 8) / 2);
+    printw("Standout");
+    attroff(A_STANDOUT);
+
     /* turn A_BOLD attribute on */
     attron(A_BOLD);
-
-    move((row - 3 ) / 2, (col - 4) / 2);
-    printw("Bold\n");
+    move((row - LINES_NO ) / 2 + 1, (col - 4) / 2);
+    printw("Bold");
 
     /* turn A_BOLD attribute off */
     attroff(A_BOLD);
-    move((row - 3) / 2 + 1, (col - 8) / 2);
-    printw("Not bold\n");
+    move((row - LINES_NO) / 2 + 2, (col - 6) / 2);
+    printw("Normal");
 
-    move((row - 3) / 2 + 2, (col - 6) / 2);
+    move((row - LINES_NO) / 2 + 3, (col - 6) / 2);
     attron(A_ITALIC);
-    printw("Italic\n");
+    printw("Italic");
     attroff(A_ITALIC);
+
+    move((row - LINES_NO) / 2 + 4, (col - 18) / 2);
+    attron(A_UNDERLINE | A_DIM);
+    printw("Underlined and dim");
+    attroff(A_UNDERLINE | A_DIM);
 
     getch();
     endwin();
